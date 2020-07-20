8724941dd9200b79eae1e17c9909a8b04dde31c6
Shivam Gambhir <40125580+TheShivamGambhir@users.noreply.github.com>
2020-07-21 04:52:25 +0530

    Added Login Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder ,FormGroup , Validators } from "@angular/forms";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mbti-test';
+  rForm: FormGroup;
+  post:any;                     // A property for our submitted form
+  description:string = '';
+  name:string = '';
+
+  constructor(private fb: FormBuilder) { 
+
+    this.rForm = fb.group({
+      'name' : [null, Validators.required],
+      'description' : [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(500)])],
+      'validate' : [null,Validators.required]
+    });
+
+  }
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule , ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -74,7 +74,9 @@ import { EsfjComponent } from './esfj/esfj.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
