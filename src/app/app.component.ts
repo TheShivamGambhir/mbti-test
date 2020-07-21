cfdeb36eaee726f04e81c829419acfcec4f1f76e
Shivam Gambhir <40125580+TheShivamGambhir@users.noreply.github.com>
2020-07-21 18:49:53 +0530

    Added Local Json Server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder ,FormGroup , Validators } from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+import data from 'db.json';   
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +10,29 @@ import { FormBuilder ,FormGroup , Validators } from "@angular/forms";
 export class AppComponent {
   title = 'mbti-test';
   rForm: FormGroup;
-  post:any;                     // A property for our submitted form
-  description:string = '';
+  post:any;                     
+  favSuperHero:string = '';
   name:string = '';
-
-  constructor(private fb: FormBuilder) { 
-
+  constructor(private fb: FormBuilder , private http:HttpClient ) { 
     this.rForm = fb.group({
       'name' : [null, Validators.required],
-      'description' : [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(500)])],
+      'favSuperHero' : [null, Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(500)])],
       'validate' : [null,Validators.required]
     });
+    
+  }
 
-  }
   addPost(post) {
-    this.description = post.description;
+    let url = "http://localhost:3000/users";
+    this.favSuperHero = post.favSuperHero;
     this.name = post.name;
+    var entry = {
+       "name" : this.name,
+       "code" : this.favSuperHero 
+    }
+    this.http.post(url,entry).toPromise().then((data)=>{
+      console.log(data);
+    });
   }
+
 }
